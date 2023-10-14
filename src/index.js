bc8d9cc2c490f0bfc1dd5b68da1ba1d4e8ca8e56
Ndindairene <irene.ndindaa@gmail.com>
2023-10-14 22:03:18 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const inputForm = document.querySelector('form');
+  inputForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+   buildToDo(event.target.children[1].value);
+   inputForm.reset();
+    });
 });
+
+function buildToDo(todo){
+ let p = document.createElement('p');
+ let btn = document.createElement('button');
+ btn.addEventListener('click',handleDelete);
+ btn.textContent = 'X'
+ p.textContent = ` ${todo} `
+ p.appendChild(btn);
+ document.querySelector("#list").appendChild(p);
+
+}
+
+function handleDelete(event) {
+  event.target.parentNode.remove()
+}
